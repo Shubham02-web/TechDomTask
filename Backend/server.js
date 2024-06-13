84a7de3e8901fd98f3cf1128ecb15352ddf474c0
Shubham Vishwakarma <vi453770@gmail.com>
2024-06-14 03:01:50 +0530

    have done  complete setup of mongoose connection and express app bodyparser for parsing json data cors and fially app listen basicsetup complete for backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,5 +4,26 @@ import mongoose from "mongoose";
 import bodyparser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 // config dotenv for Load Environment variables from .env file
 dotenv.config();
+
+// Creating setup of an express application
+const app = express();
+
+// Enabling Cors for all routes
+app.use(cors());
+// use express Json for parsing incoming json requests
+app.use(bodyParser.json());
+
+// Connecting MongoDB
+mongoose
+  .connect(process.env.MongoURI)
+  .then(() => console.log("MongoDb has Connected"))
+  .catch((err) => console.log(`Error in conecting mongoose ${err}`));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`app listening on PORT ${PORT}`);
+});
