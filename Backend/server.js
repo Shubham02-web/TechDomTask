656b68086dc07853e54f236eb46603b41ca2381e
Shubham Vishwakarma <vi453770@gmail.com>
2024-06-14 02:40:56 +0530

    setup complete for backend part of the loan application  and now working on server.js our main file

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+// importing Necessary Modules
+import express from "express";
+import mongoose from "mongoose";
+import bodyparser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+// config dotenv for Load Environment variables from .env file
+dotenv.config();
